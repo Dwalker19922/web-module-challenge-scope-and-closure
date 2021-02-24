182b6d4199d6dac9892b323f1a4100e3e27c8b1d
Daniel Walker <Dw765608@gmail.com>
2021-02-23 20:51:56 -0500

    final

--- index.js
+++ index.js
@@ -28,11 +28,16 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  counter one is self-contained local scope
   
   2. Which of the two uses a closure? How can you tell?
+
+  counter two because it uses a global variable
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     i prefer the counter1 local scope
 */
 
 // counter1 code
@@ -62,10 +67,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-\function inning(/*Code Here*/){
+function inning(/*Code Here*/){
   return Math.floor(Math.random() * Math.floor(2));
 }
 console.log(inning())
+
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -99,7 +105,6 @@ console.log(finalScore(inning, 9));
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
-
   function getInningScore(inningcb) {
     let homeScore = inning();
     let awayScore = inning();
@@ -109,7 +114,6 @@ Use the getInningScore() function below to do the following:
      
     }
   }
-
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -151,7 +155,6 @@ Use the scoreboard function below to do the following:
   "This game will require extra innings: Away 10 - Home 10"
 ]  
   */
-
  function scoreboard() {
   let scorelist = []
   let homeScore=0;
@@ -164,6 +167,10 @@ Use the scoreboard function below to do the following:
    
 return scorelist
  }
+  
+
+  
+
 
 
 
